fb65947fc35ee43b29cafa05f091994647bc5978
Wilton Garcia <wiltongarciaweb@gmail.com>
2022-03-30 14:26:12 -0300

    Criação do projeto

---
+++ WGPokdex/WGPokdex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WGPokdex
+//
+//  Created by Wilton Garcia on 30/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WGPokdex/WGPokdex/WGPokdexApp.swift
@@ -0,0 +1,17 @@
+//
+//  WGPokdexApp.swift
+//  WGPokdex
+//
+//  Created by Wilton Garcia on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WGPokdexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
